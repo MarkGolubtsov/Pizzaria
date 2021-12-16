9edfa5475eb2acff1bd4bc909f4e54335a0a1903
Mark <glmarkgl4@gmail.com>
2021-12-16 20:41:43 +0300

    Added private page layout.

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import 'antd/dist/antd.css';
 import {Navigate, Route, Routes} from 'react-router-dom';
 import LoginPage from 'app/logic/login/LoginPage';
 import PizzaOrdersPage from 'app/logic/PizzaOrdersPage';
 import AuthProvider from 'app/AuthProvider';
+import PrivatePageLayout from 'app/logic/PrivatePageLayout';
 import './App.css';
-import 'antd/dist/antd.css';
 
 function App() {
     return (
         <AuthProvider>
             <Routes>
                 <Route path='login' element={<LoginPage/>}/>
-                <Route path='pizza-orders' element={<PizzaOrdersPage/>}/>
+                <Route path='pizza-orders' element={<PrivatePageLayout/>}>
+                    <Route index element={<PizzaOrdersPage/>}/>
+                </Route>
                 <Route path='*' element={<Navigate to='/login'/>}/>
             </Routes>
         </AuthProvider>
--- frontend/src/app/logic/PizzaOrdersPage.tsx
+++ frontend/src/app/logic/PizzaOrdersPage.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import useAuth from 'app/useAuth';
-
 
 export default function PizzaOrdersPage() {
-    const {logout} = useAuth();
-
     return (
         <div className='pizza-orders'>
             Pizza orders
-            <span onClick={logout}>Logout</span>
         </div>
     )
 }
