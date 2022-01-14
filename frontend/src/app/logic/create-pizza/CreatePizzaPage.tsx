a5d5823f43c4cb394167eac09eb73d10d5113837
Mark <glmarkgl4@gmail.com>
2022-01-14 12:53:09 +0300

    fix create form styles.

--- frontend/src/app/logic/create-pizza/CreatePizzaPage.tsx
+++ frontend/src/app/logic/create-pizza/CreatePizzaPage.tsx
@@ -11,19 +11,33 @@ export default function CreatePizzaPage() {
 
     return (
         <div className='create-pizza-page'>
-            <AppForm className='create-pizza-form' layout="vertical" form={form} onFinish={handleCreate}>
-                <Form.Item name='name' label='Name' rules={[{required: true, message: 'Provide Name'}]}>
+            <AppForm className='create-pizza-form' layout="vertical" form={form} onFinish={handleCreate}  style={{
+                marginTop: 20
+            }}>
+                <Form.Item style={{
+                    width: 500
+                }} name='name' label='Name' rules={[{required: true, message: 'Provide Name'}]}>
                     <Input/>
                 </Form.Item>
-                <Form.Item name='description' label='Description'
+                <Form.Item style={{
+                    width: 500
+                }} name='description' label='Description'
                            rules={[{required: true, message: 'Provide Description'}]}>
                     <TextArea/>
                 </Form.Item>
-                <Form.Item name='price' label='Price' rules={[{required: true, message: 'Provide Price'}]}>
-                    <InputNumber min={1} max={10000}/>
+                <Form.Item style={{
+                    width: 500
+                }} name='price' label='Price' rules={[{required: true, message: 'Provide Price'}]}>
+                    <InputNumber min={1} max={10000}  style={{
+                        width: 500
+                    }}/>
                 </Form.Item>
-                <Form.Item name='weight' label='Weight' rules={[{required: true, message: 'Provide Weight'}]}>
-                    <InputNumber min={1} max={10000}/>
+                <Form.Item name='weight' label='Weight' rules={[{required: true, message: 'Provide Weight'}]} style={{
+                    width: 500
+                }}>
+                    <InputNumber min={1} max={10000}  style={{
+                        width: 500
+                    }}/>
                 </Form.Item>
                 <Form.Item>
                     <Button htmlType='submit'>
